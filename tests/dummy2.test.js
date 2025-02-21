49c6cbac5aaabe8067c6a846998c279e4bf2e644
Максим Оглобля <maxogloblia@gmail.com>
2025-02-21 18:49:17 +0200

    dummy tests

---
+++ tests/dummy2.test.js
@@ -0,0 +1,7 @@
+import assert from 'assert';
+
+describe('Second Dummy Test', function () {
+  it('повинен завжди проходити (2 === 2)', function () {
+    assert.strictEqual(2, 2);
+  });
+});
---
+++ tests/dummy3.test.js
@@ -0,0 +1,7 @@
+import assert from 'assert';
+
+describe('Third Dummy Test', function () {
+  it('повинен завжди проходити (hello === hello)', function () {
+    assert.strictEqual('hello', 'hello');
+  });
+});
